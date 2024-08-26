96b7d156054e7299bdf79604a85c7fe4d517b129
Нафиков Ильхам <snkr.nk95@gmail.com>
2024-08-26 15:43:20 +0300

    add lint staged

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,6 +1,26 @@
+// Требуемый формат коммита:
+// feat(scope): description
 module.exports = {
-    extends: ['@commitlint/config-conventional'],
-    rules: {
-        'scope-empty': [2, 'never']
-    }
+  rules: {
+    'body-leading-blank': [2, 'always'],
+    'body-max-line-length': [2, 'always', 100],
+    'footer-leading-blank': [2, 'always'],
+    'footer-max-line-length': [2, 'always', 100],
+    'header-max-length': [2, 'always', 100],
+    'header-full-stop': [2, 'never', '.'],
+    'subject-empty': [2, 'never'],
+    'type-case': [2, 'always', 'lower-case'],
+    'type-empty': [2, 'never'],
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat', // Фича
+        'fix', // Исправление бага
+        'refactor', // Рефакторинг кода
+        'chore', // Изменение файлов, которые не являются частью проекта (Конфиги, библиотеки)
+        'docs' // исправление документации
+      ]
+    ]
+  }
 };
